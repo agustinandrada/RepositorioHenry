68bc53d3df917c3ef972f9998956cb4c226446b0
Huemul <agustinandrada1@gmail.com>
2023-02-01 11:46:29 -0300

    la que va

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+// var listaDeCompras = [];
+// listaDeCompras[3]='tomates';
+// listaDeCompras[1]='lechuga';
+
+//  console.log(listaDeCompras)
+
+// var colores = ['amarillo', 'azul'];
+// colores.push('rojo');
+// colores.unshift('verde');
+// colores.pop();
+// colores.shift();
+
+// console.log(colores);
---
+++ Bucles.js
@@ -0,0 +1,17 @@
+//bucle for
+var suma = 0;
+
+ for(var i=0 ; i<5; i ++){
+suma = suma +i; 
+   console.log('Variable de iteracion: ', i);
+}
+console.log('Variable Suma: ', suma);
+
+
+//bucle while
+
+while(suma < 3){
+    suma = suma +1;
+    console.log(suma);
+ }
+ 
